212d2363f35e1e23db2ceac89a13d366a932da4b
mkoenke <mkoenke@sas.upenn.edu>
2020-11-14 17:57:56 -0500

    completed

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = Math.floor( Math.random() * 1000 ) + 1
+const mod = 18 % 7
+const max = Math.max(3, 5, 7, 19, 20)
